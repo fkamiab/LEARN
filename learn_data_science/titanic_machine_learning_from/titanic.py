7a20ce28c266bc921f547ba9067b790bd3fa703d
fkamiab <Farbod.Kamiab@asset-metrix.com>
2017-01-30 10:29:47 +0100

    Learning machine learning techniques in python using a dataset about the Titanic

---
+++ learn_data_science/titanic_machine_learning_from/titanic.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from pandas import Series, DataFrame
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# useful statistical data visualization package
+import seaborn as sns
+sns.set_style('whitegrid')
+%matplotlib inline
+
+# machine learning packages
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+
+
